2dd1170e5bc4a3e04f9e1655d4e1cc5c0d69e0d6
nikogriffs <nikogriffs@yandex.ru>
2021-04-14 18:26:36 +0700

    Сделал открытие по картинке и плавность

--- scripts/script.js
+++ scripts/script.js
@@ -24,48 +24,44 @@ const cards = [{
     }
 ];
 
-
-
 const profile = document.querySelector('.profile');
 const popupEdit = document.querySelector('.popup__edit');
 const popupAdd = document.querySelector('.popup__add');
+const popupCard = document.querySelector('.popup__card');
+const popupImageCard = popupCard.querySelector('.popup__image');
+const popupCaptionCard = popupCard.querySelector('.popup__caption');
 const places = document.querySelector('.places__list');
 const template = places.querySelector('.places__card-template').content;
 
-
-
-
-
-
-
-// Находим кнопки "Редактировать" и "Закрыть"
+// Находим кнопки
 const editButton = profile.querySelector('.profile__edit-button');
 const addButton = profile.querySelector('.profile__add-button');
 const closeEditButton = popupEdit.querySelector('.popup__close-button');
 const closeAddButton = popupAdd.querySelector('.popup__close-button');
-
+const closeImageCard = popupImageCard.querySelector('.popup__close-button');
 
 // Находим поля "Имя" и "О себе"
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
 
-// Находим форму
+// Находим формы
 const formEditElement = popupEdit.querySelector('.popup__form');
 const formAddElement = popupAdd.querySelector('.popup__form');
 
 // Находим поля формы
-const nameInput = formEditElement.querySelector('.popup__input_title_name'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formEditElement.querySelector('.popup__input_title_job'); // Воспользуйтесь инструментом .querySelector()
+const nameInput = formEditElement.querySelector('.popup__input_title_name');
+const jobInput = formEditElement.querySelector('.popup__input_title_job');
 const placeInput = formAddElement.querySelector('.popup__input_title_place');
 const linkInput = formAddElement.querySelector('.popup__input_title_link');
 
-function clickLikeButton(like) {
-    like.target.classList.toggle('places__like-button_active');
+function popupOpened(popup) {
+    popup.classList.add('popup_opened');
 }
 
-function clickTrashButton(trash) {
-    trash.target.closest('.places__card').remove();
+function popupClosed(popup) {
+    popup.classList.remove('popup_opened');
 }
+
 
 function createCard(card) {
     const cardTemplate = template.querySelector('.places__card').cloneNode(true);
@@ -74,68 +70,70 @@ function createCard(card) {
     cardTemplate.querySelector('.places__image').alt = card.name;
     cardTemplate.querySelector('.places__like-button').addEventListener('click', clickLikeButton);
     cardTemplate.querySelector('.places__trash-button').addEventListener('click', clickTrashButton);
+    cardTemplate.querySelector('.places__image').addEventListener('click', clickImage);
     return cardTemplate;
 }
 
-cards.forEach(function(card) {
-    places.append(createCard(card));
+function clickImage(evt) {
+    popupCaptionCard.textContent = evt.target.alt;
+    popupImageCard.src = evt.target.src;
+    popupImageCard.alt = evt.target.alt;
+    popupOpened(popupCard);
+}
+
+cards.forEach(function(evt) {
+    places.append(createCard(evt));
 });
 
-
-
-// Функция для открытия редактирования
+// Функция для лайка
+function clickLikeButton(evt) {
+    evt.target.classList.toggle('places__like-button_active');
+}
+// Функция для удаления карточки
+function clickTrashButton(evt) {
+    evt.target.closest('.places__card').remove();
+}
+// Функция для открытия формы редактирования
 function clickEditButton() {
-    popupEdit.classList.add('popup_opened');
+    popupOpened(popupEdit);
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
-
+// Функция для открытия формы добавления
 function clickAddButton() {
-    popupAdd.classList.add('popup_opened');
+    popupOpened(popupAdd);
 }
 
-// Функция для закрытия
+function clickCloseCard() {
+    popupClosed(popupCard);
+}
+// Функция для закрытия формы редактирования
 function clickCloseEditButton() {
-    popupEdit.classList.remove('popup_opened');
+    popupClosed(popupEdit);
 }
-
+// Функция для закрытия формы добавления
 function clickCloseAddButton() {
-    popupAdd.classList.remove('popup_opened');
+    popupClosed(popupAdd);
 }
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Обработчик «отправки» формы в окне редактирования
 function formEditSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     clickCloseEditButton();
 }
-
+// Обработчик «отправки» формы в окне добавления
 function formAddSubmitHandler(evt) {
     evt.preventDefault();
-
     clickCloseAddButton();
 }
-
-
-
-
-//Слушатели клика по кнопке для открытия и закрытия окна редактирования
-
+//Слушатели клика по кнопке для открытия и закрытия форм
+popupCard.addEventListener('click', clickCloseCard);
 editButton.addEventListener('click', clickEditButton);
 addButton.addEventListener('click', clickAddButton);
 closeEditButton.addEventListener('click', clickCloseEditButton);
 closeAddButton.addEventListener('click', clickCloseAddButton);
-// Прикрепляем обработчик к форме:
+// Прикрепляем обработчик к формам:
 // он будет следить за событием “submit” - «отправка»
 formEditElement.addEventListener('submit', formEditSubmitHandler);
 formAddElement.addEventListener('submit', formAddSubmitHandler);
