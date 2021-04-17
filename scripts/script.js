a8997c68ebc964e618c8bf42d054e2c2eab2fc84
nikogriffs <nikogriffs@yandex.ru>
2021-04-17 19:42:31 +0700

    Вынес initial-cards в отдельный скрипт

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,3 @@
-const cards = [{
-  name: 'Архыз',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-},
-{
-  name: 'Челябинская область',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-},
-{
-  name: 'Иваново',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-},
-{
-  name: 'Камчатка',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-},
-{
-  name: 'Холмогорский район',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-},
-{
-  name: 'Байкал',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-}
-];
-
 // Находим необходимые элементы для дальнейшей работы в ДОМ
 const profile = document.querySelector('.profile');
 const popupEdit = document.querySelector('.popup-edit');
