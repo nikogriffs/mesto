7c28017ee692f1acada6d44b969e131afb2144d3
nikogriffs <nikogriffs@yandex.ru>
2021-05-20 23:21:42 +0700

    сделал класс Попап

--- scripts/index.js
+++ scripts/index.js
@@ -56,21 +56,30 @@ class Popup {
   }
 
   open() {
-    this._popup.classList.add(.);
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keyup', this._handleEscClose);
   }
 
-  close() {
-    this._popup.classList.add(.);
+  close = () => {
+    this._popup.classList.remove('.popup_opened');
+    document.addEventListener('keyup', this._handleEscClose);
   }
 
   _handleEscClose(e) {
-
+    if(e.keyCode === 27)
+    this.close();
   }
 
   setEventListeners() {
-    this._popup.querySelector(.).addEL(() => {
+    this._popup.querySelector('.popup__close-button').addEL(() => {
       this.close();
     });
+  }
+}
+
+class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
   }
 }
 
